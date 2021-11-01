c606af2a975bbf6dbb3c82dcde35634d3b60799b
Maxim Lebedev <lmax942@gmail.com>
2021-11-01 19:39:50 +0300

    add Login VC

---
+++ Messenger/LoginViewController.swift
@@ -0,0 +1,126 @@
+//
+//  LoginViewController.swift
+//  Messenger
+//
+//  Created by Максим Лебедев on 01.11.2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    let welcomeLabel = UILabel(text: "Welcome back!", font: .avenir26())
+    
+    let loginWithLabel = UILabel(text: "Login with")
+    let orLabel = UILabel(text: "or")
+    let emailLabel = UILabel(text: "Email")
+    let passwordLabel = UILabel(text: "Password")
+    let needAnAccountLabel = UILabel(text: "Need an account?")
+    
+    let googleButton = UIButton(title: "Google",
+                                titleColor: .black,
+                                backgroundColor: .white,
+                                isShadow: true)
+    let emailTextField = OneLineTextField(font: .avenir20())
+    let passwordTextField = OneLineTextField(font: .avenir20())
+    
+    let loginButton = UIButton(title: "Login", titleColor: .white, backgroundColor: .buttonBlack(), cornerRadius: 4)
+    
+    let signUpButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Sign Up", for: .normal)
+        button.setTitleColor(.buttonRed(), for: .normal)
+        button.titleLabel?.font = .avenir20()
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
+        setupConstraints()
+    }
+}
+
+// MARK: - Setup Constraints
+
+extension LoginViewController {
+    private func setupConstraints() {
+        let loginWithView = ButtonFormView(label: loginWithLabel, button: googleButton)
+        let emailStackView = UIStackView(arrangedSubviews: [emailLabel, emailTextField],
+                                         axis: .vertical,
+                                         spacing: 0)
+        let passwordStackView = UIStackView(arrangedSubviews: [passwordLabel, passwordTextField],
+                                            axis: .vertical,
+                                            spacing: 0)
+        
+        loginButton.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        
+        let stackView = UIStackView(arrangedSubviews: [
+            loginWithView,
+            orLabel,
+            emailStackView,
+            passwordStackView,
+            loginButton
+        ],
+                                            axis: .vertical,
+                                            spacing: 40)
+        
+        signUpButton.contentHorizontalAlignment = .leading
+        let bottomStackView = UIStackView(arrangedSubviews: [needAnAccountLabel, signUpButton],
+                                          axis: .horizontal,
+                                          spacing: 10)
+        bottomStackView.alignment = .firstBaseline
+        
+        welcomeLabel.translatesAutoresizingMaskIntoConstraints = false
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        bottomStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(welcomeLabel)
+        view.addSubview(stackView)
+        view.addSubview(bottomStackView)
+        
+        NSLayoutConstraint.activate([
+            welcomeLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 160),
+            welcomeLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: welcomeLabel.bottomAnchor, constant: 80),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
+        ])
+        
+        NSLayoutConstraint.activate([
+            bottomStackView.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 60),
+            bottomStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            bottomStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
+        ])
+    }
+}
+
+
+// MARK: - SwiftUI
+
+import SwiftUI
+
+struct LoginVCProvider: PreviewProvider {
+    static var previews: some View {
+        ContainerView().edgesIgnoringSafeArea(.all)
+    }
+    
+    struct ContainerView: UIViewControllerRepresentable {
+        
+        let loginVC = LoginViewController()
+        
+        func makeUIViewController(context: UIViewControllerRepresentableContext<LoginVCProvider.ContainerView>) -> LoginViewController {
+            return loginVC
+        }
+        
+        func updateUIViewController(_ uiViewController: UIViewControllerType, context: UIViewControllerRepresentableContext<LoginVCProvider.ContainerView>) {
+            
+        }
+    }
+}
+
